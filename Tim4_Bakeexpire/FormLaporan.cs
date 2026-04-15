97894f650ead8bef314d23f858f5436268052a3c
niel55-stack <vylingsniel55@gmail.com>
2026-04-15 12:12:52 +0700

    mengganti application run pada program menjadi form login

--- Tim4_Bakeexpire/FormLaporan.cs
+++ Tim4_Bakeexpire/FormLaporan.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace Tim4_Bakeexpire
 {
-    public partial class Form1: Form
+    public partial class FormLaporan: Form
     {
-        public Form1()
+        public FormLaporan()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
