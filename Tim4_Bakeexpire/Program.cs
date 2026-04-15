50cc9c734518b379b0f0efc0113a2e09dbfa3b54
niel55-stack <vylingsniel55@gmail.com>
2026-04-15 13:02:35 +0700

    .

--- Tim4_Bakeexpire/Program.cs
+++ Tim4_Bakeexpire/Program.cs
@@ -16,7 +16,7 @@ namespace Tim4_Bakeexpire
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMonitoring());
+            Application.Run(new FormLogin());
         }
     }
 }
