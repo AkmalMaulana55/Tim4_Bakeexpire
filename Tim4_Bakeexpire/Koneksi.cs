e5185e95011dca4f497df2ce95cc535c657641d5
niel55-stack <vylingsniel55@gmail.com>
2026-04-15 11:21:30 +0700

    Add project files.

---
+++ Tim4_Bakeexpire/Koneksi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tim4_Bakeexpire
+{
+    class Koneksi
+    {
+        return new SqlConnection("Data Source=.;Initial Catalog=db_bahan_kue;Integrated Security=True");
+    }
+}
